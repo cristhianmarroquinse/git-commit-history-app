ba7bc294161909348690e371e5c36a9ad9660706
Cristhian Marroquin <cristhianmarroquinse@gmail.com>
2023-04-11 18:18:50 -0500

    remove axios and create getCommit method at GithubService

--- backend/src/github/github.controller.ts
+++ backend/src/github/github.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
+import { GithubService } from './github.service';
 
-@Controller('github')
-export class GithubController {}
+@Controller('repositories')
+export class GithubController {
+  constructor(private readonly githubService: GithubService) {}
+
+  @Get(':owner/:repo/commits')
+  async getCommits(@Param('owner') owner: string, @Param('repo') repo: string) {
+    const commits = await this.githubService.getCommits(owner, repo);
+    return commits;
+  }
+}
--- backend/src/github/github.service.ts
+++ backend/src/github/github.service.ts
@@ -1,4 +1,27 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, HttpException, HttpStatus } from '@nestjs/common';
+import { Octokit } from '@octokit/rest';
 
 @Injectable()
-export class GithubService {}
+export class GithubService {
+  private readonly octokit: Octokit;
+  
+  constructor() {
+    this.octokit = new Octokit({
+      auth: `token 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`,
+    });
+  }
+  
+  async getCommits(owner: string, repo: string) {
+    try {
+      const response = await this.octokit.repos.listCommits({
+        owner,
+        repo,
+      });
+      return response.data;
+    } catch (error) {
+      const status = error.status || HttpStatus.INTERNAL_SERVER_ERROR;
+      const message = error.message || 'Internal Server Error';
+      throw new HttpException(message, status);
+    }   
+  }
+}
