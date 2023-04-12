0306b50a9c2ad76e693e8a0aa927611beaaf373e
Cristhian Marroquin <cristhianmarroquinse@gmail.com>
2023-04-12 13:58:26 -0500

    improve styles

--- frontend/src/components/CommitHistory.tsx
+++ frontend/src/components/CommitHistory.tsx
@@ -1,5 +1,7 @@
 import { useCommitHistory } from '../hooks/useCommitHistory';
 import Commit from '../interfaces/Commit';
+import './styles.css';
+import moment from 'moment';
 
 interface CommitHistoryProps {
   owner: string;
@@ -18,19 +20,21 @@ const CommitHistory = ({ owner, repo }: CommitHistoryProps) => {
   }
 
   return (
-    <div>
+    <ul className="commit-list">
       {commits.map((commit: Commit) => (
-        <div key={commit.sha}>
-          <div>{commit.sha}</div>
-          <div>{commit.author}</div>
-          <div>{commit.author_avatar}</div>
-          <div>{commit.author_name}</div>
-          <div>{commit.message}</div>
-          <div>{commit.date}</div>
-          <div>{commit.url}</div>
-        </div>
+        <li key={commit.sha} className="commit-item">
+          <div className="commit-message-container">
+            <a href={commit.url} className="commit-message" target="_blank">{commit.message}</a>
+            <div className="commit-meta">
+              <img src={commit.author_avatar} alt={`${commit.author}'s avatar`} className="commit-avatar" />
+              <a href={`https://github.com/${commit.author}`} className="commit-author" target="_blank">{commit.author}</a>
+              <span>{`<${commit.author_name}>`}</span>
+              <span className="commit-date">{moment(commit.date).fromNow()}</span>
+            </div>
+          </div>
+        </li>
       ))}
-    </div>
+    </ul>
   );
 };
 
